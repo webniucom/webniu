fddd30f1bf64c056ff5044eb66f9f19757c255b5
网牛 <xhl1988615@163.com>
2025-09-08 16:55:55 +0800

    v1.1.1

--- src/plugin/webniu/app/support/rewrite.php
+++ src/plugin/webniu/app/support/rewrite.php
@@ -3,9 +3,5 @@ return [
     '/webniu'   => [
         plugin\webniu\app\web\controller\IndexController::class,
         'index'
-    ],
-    '/index/vuemc.vue'   => [
-        app\controller\IndexController::class,
-        'vuemc'
     ]
 ];
